d33b0bf90fec125a5f8eb71c998836999e166001
Diego <soterg.uhdeia@gmail.com>
2021-05-17 17:53:55 -0500

    edit streams

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,40 @@
-import React from "react";
+import _ from "lodash";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+const StreamEdit = ({ fetchStream, match, stream, editStream }) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, [fetchStream]);
+
+  const onSubmit = (formValues) => {
+    editStream(match.params.id, formValues);
+  };
+
+  return (
+    <div>
+      {!stream ? (
+        "Loading..."
+      ) : (
+        <div>
+          <h3>Edit a Stream</h3>
+          <StreamForm
+            initialValues={_.pick(stream, "title", "description")}
+            onSubmit={onSubmit}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
-export default StreamEdit;
+//ownProps son los props que se pasan del componente StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -12,8 +12,12 @@ const StreamList = ({ fetchStreams, streams, currentUserId, isSignedIn }) => {
     if (stream.userId === currentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
-          <button className="ui button negative">Delete</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link to="/" className="ui button negative">
+            Delete
+          </Link>
         </div>
       );
     }
