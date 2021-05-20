3feecfd092c9c0b7ae34372d4f027b220d319df2
Diego <soterg.uhdeia@gmail.com>
2021-05-20 00:15:00 -0500

    streaming videos

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,33 @@
 import React, { useEffect } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 const StreamShow = ({ match, fetchStream, stream }) => {
+  let player;
   useEffect(() => {
     fetchStream(match.params.id);
+    buildPlayer();
+
+    return () => {
+      player.destroy();
+    };
   }, [fetchStream, match.params.id]);
+
+  const buildPlayer = () => {
+    if (!stream) {
+      return;
+    }
+    player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${match.params.id}.flv`,
+    });
+    console.log(player);
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
+
+  const videoRef = React.createRef();
 
   return (
     <div>
@@ -13,6 +35,7 @@ const StreamShow = ({ match, fetchStream, stream }) => {
         "Loading..."
       ) : (
         <div>
+          <video ref={videoRef} style={{ width: "100%" }} controls />
           <h1>{stream.title}</h1>
           <h5>{stream.description}</h5>
         </div>
